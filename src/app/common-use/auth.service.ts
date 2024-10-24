6978564bfc3cc711774e7e4fd2987b5ac5520af2
SAAFDataEngineers <saafdataengineers@gmail.com>
2024-10-24 22:14:20 +0200

    loginpage update

--- src/app/common-use/auth.service.ts
+++ src/app/common-use/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { JwtHelperService  } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
@@ -9,33 +8,20 @@ import { JwtHelperService  } from '@auth0/angular-jwt';
 export class AuthService {
 
   constructor( private http: HttpClient,
-    private jwtHelper: JwtHelperService
+    //private jwtHelper: JwtHelperService,
+    
+
    ) {}
 
   apiUrl: string='localhost:4200';
 
-  loginUser(){
-
-  }
 
 
   public isAuthenticated() : boolean {
         
     const token = sessionStorage.getItem('authToken');
-    
-    const isExpired = this.jwtHelper.isTokenExpired(token);
-    console.log("suc: ",token, "exp:", isExpired)
-    
-    if (token==='success')
-    {
-      console.log("isAuthen suc: ",token, "exp:", sessionStorage.getItem('authToken'))
-      return true
-    }
-    
-    return false
-
-    
-    //return !isExpired;
+    return token==='success' ? true : false
+        
   }
 
 
